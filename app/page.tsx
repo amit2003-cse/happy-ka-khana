b3906336804fea2d38713f04e1ca580eb209a8c4
Amit Kumar <amit4321sg@gmail.com>
2026-08-15 00:57:06 +0530

    style: reorder landing page sections to optimize conversion funnel

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
         <HeroSection />
         <WeeklyMenu />
         <PricingPlans />
-        <WhyChooseUs />
         <Testimonials />
+        <WhyChooseUs />
         <DeliveryAreas />
         <FAQ />
       </main>
